d54c1f3c12f7abe801c0df1bbfee613b67a0d119
Dishant Singh <65276766+dishantsingh1998@users.noreply.github.com>
2021-03-22 16:20:44 +0530

    first commit

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+
+import Product from './Product/Product';
+import useStyles from './styles';
+
+const Products = ({ products, onAddToCart }) => {
+  const classes = useStyles();
+
+  if (!products.length) return <p>Loading...</p>;
+
+  return (
+    <div>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} onAddToCart={onAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+    </div>
+  );
+};
+
+export default Products;
+
